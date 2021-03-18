81b23560c0838077fe5a68ba2a174f68ae159085
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-03-18 16:52:35 -0400

    now CartContext is giving data to navigation and shopping cart

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Navigation = props => {
+import CartContext from '../contexts/CartContext';
+
+// const Navigation = props => {} ---> This was given
+const Navigation = () => {
+	const cart = useContext(CartContext);
+
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
 			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
+				{/* Cart <span>{props.cart.length}</span> ---> This was given */}
+				Cart <span>{cart.length}</span>
 			</NavLink>
 		</div>
 	);
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+//Context
+import CartContext from '../contexts/CartContext';
 
 // Components
 import Item from './ShoppingCartItem';
 
-const ShoppingCart = props => {
+// const ShoppingCart = props => { ---> this was given
+const ShoppingCart = () => {
+	const cart = useContext(CartContext);
+
 	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
+		// return props.cart.reduce((acc, value) => { ---> This was given
+		return cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
 
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
+			{/* {props.cart.map(item => ( ---> This was given */}
+			{cart.map(item => (
 				<Item key={item.id} {...item} />
 			))}
 
